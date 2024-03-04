7d5b3c0790feac8834e37ba2620fa681a24100e5
Lucaskkkj <lucasdepauloldp1@gmail.com>
2024-03-04 20:43:40 -0300

    .

--- database/config/config.js
+++ database/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 module.exports = {
 	"development": {
 		"username": process.env.DB_USER,
-		"password": null,
+		"password": process.env.DB_PASSWORD,
 		"database": process.env.DB_DATABASE,
 		"port": process.env.DB_PORT,
 		"host": process.env.DB_HOST,
