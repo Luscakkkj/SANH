cf88482d9282446af23bad55ca600540f589f843
Lucaskkkj <lucasdepauloldp1@gmail.com>
2024-03-04 20:54:50 -0300

    .

--- database/config/config.js
+++ database/config/config.js
@@ -8,6 +8,12 @@ module.exports = {
 		"port": process.env.DB_PORT,
 		"host": process.env.DB_HOST,
 		"dialect": "mysql",
+		"pool": {
+			"max": 50,
+			"min": 0,
+			"acquire": 30000,
+			"idle": 10000
+    },
 		"define": {
 			"timestamps": false,
 			"underscore": true
